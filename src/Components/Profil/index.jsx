3032685d1207235b2aa1182a8a5d475955b1c4e9
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-05-23 16:32:57 +0200

    User Storie-6 : Activité quotidienne

--- src/Components/Profil/index.jsx
+++ src/Components/Profil/index.jsx
@@ -1,13 +1,14 @@
+
 import { react } from 'react'
 import './style.scss'
 import {useApiUser} from '../../Api/Api'
 
 export default function Profil ({firstName}){
-
+  
    const {data} = useApiUser(12)
-   console.log(data)
    return(<header>
-         <h1 className='header__name'>Bonjour <span>{firstName}</span></h1>
+         <h2 className='header__name'>Bonjour <span>{firstName}</span></h2>
+         <p className="header__paragraphe">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
 
    </header>)
 }
