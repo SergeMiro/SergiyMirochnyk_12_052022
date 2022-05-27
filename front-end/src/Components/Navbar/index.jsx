36bf97168031ca0ca9f6ee180c67742d9eb66b20
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-05-27 18:26:16 +0200

    Dernieres corrections + ajout commentaires

--- front-end/src/Components/Navbar/index.jsx
+++ front-end/src/Components/Navbar/index.jsx
@@ -2,20 +2,32 @@ import { NavLink, Link } from 'react-router-dom'
 import './Navbar.scss';
 import { ReactComponent as NavbarLogo } from '../../assets/images/SportSeeLogo.svg'
 
-export default function Navbar(){
+/**
+ * @description Component for showing Navbar navigation.
+ *
+ * @component
+ * @example
+ * 
+ * return(<Navbar  />)
+ */
 
+function Navbar(){
+   
    return (<header className="navbar">
-            <Link to="/">
-               <NavbarLogo className="navbar__logo"  alt="logo Sport see" />
+            <Link to="/" className="navbar__logo" >
+               <NavbarLogo  alt="logo Sport see" />
             </Link> 
+           
             <nav className="navbar__nav">
                <NavLink className="navbar__nav__link" to='/' >Acceuil</NavLink>
                <NavLink className="navbar__nav__link" to='/profil' >Profil</NavLink>
                <NavLink className="navbar__nav__link" to='/reglage' >Réglage</NavLink>
                <NavLink className="navbar__nav__link" to='/communite' >Communité</NavLink>
             </nav>
-
+            
          </header>
       );
 
 }
+
+export default  Navbar
